7a108f13bb0f255343de1475ac6cdecbde54dd08
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2024-01-27 16:33:45 +0530

    Fix incorrect user redirection after login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,16 @@
 import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 export default async function loginPage() {
 
     const session = await getServerSession(authOptions);
-    
+
+    if(session) {
+        return redirect("/account");
+    }
+
     return (
         <div>
             <div className="p-4 max-w-xs mx-auto">
